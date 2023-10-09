6d583c730fa01a32f24dcc9a60aa5e0625a6aeb7
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-10-09 14:24:22 +0300

    adding Example component

---
+++ src/example/Example.jsx
@@ -0,0 +1,11 @@
+import styles from "./Example.module.css";
+
+const Example = () => {
+  return (
+    <main className={styles.container}>
+      <p className={styles.example}>Example</p>
+    </main>
+  )
+}
+
+export default Example;
