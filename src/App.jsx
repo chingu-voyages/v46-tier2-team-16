cfbd507774220a1106be3d24d5027de3885773de
mladen.krstin <mladen.krstin@gmail.com>
2023-10-10 09:42:43 +0200

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { Example } from './example';
-import { Card } from './components/recepie-card/Card';
+// import { Card } from './components/recepie-card/Card';
 
 import './App.css';
 
 const App = () => {
     return (
         <>
-            <header>
-                <Example />
-            </header>
-            <main>
-                <Card />
-            </main>
+            {/* <Card /> */}
+            <Example />
         </>
     );
 };
