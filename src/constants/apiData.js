f2dc70545ff7ee9d4e8671197cd611c123c58a86
Nisha <nishavijai@gmail.com>
2023-10-13 07:49:00 +0200

    Add env

--- src/constants/apiData.js
+++
@@ -1,3 +0,0 @@
-export const TASTY_RAPID_API_URL = "https://tasty.p.rapidapi.com/recipes/list"
-export const TASTY_RAPID_API_KEY = "eea35795aamsh011f505e708eeb4p16ce66jsn3194d1e0f34f"
-export const TASTY_RAPID_API_HOST = "tasty.p.rapidapi.com"
