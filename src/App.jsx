ea03e77288adf6869e3a7bae1ce0c9de136cc8a0
mladen.krstin <mladen.krstin@gmail.com>
2023-10-16 22:56:50 +0200

    Added styling to recipe cards and recipe details

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,40 @@
 // import Card from './components/recipeCard/RecipeCard';
 // import Data from './data/recipes';
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import {Home} from "./pages/home";
-import {RecipeItem} from "./components/example/recipeItem";
+import { Home } from './pages/home';
+import { RecipeItem } from './components/example/recipeItem';
 import './App.css';
-
-
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
-    //   errorElement: <NotFound />,
+        path: '/',
+        element: <Home />,
+        //   errorElement: <NotFound />,
     },
     {
-        path:"/recipe/:id",
-        element: <RecipeItem/>
-    }
-  ]);
-
-
+        path: '/recipe/:id',
+        element: <RecipeItem />,
+    },
+]);
 
 const App = () => {
     return (
         <>
-            {/* <BrowserRouter>
-                <div className="container">
-                    <div className="card">
-                        <Card items={Data} />
-                    </div>
-                </div>
-            </BrowserRouter> */}
-             <RouterProvider router={router} />
+            <RouterProvider router={router} />
         </>
     );
 };
 
 export default App;
+
+{
+    /* <BrowserRouter>
+                <div className="container">
+                    <div className="card">
+                        <Card items={Data} />
+                    </div>
+                </div>
+            </BrowserRouter> */
+}
--- src/components/example/recipeList/RecipeList.jsx
+++ src/components/example/recipeList/RecipeList.jsx
@@ -1,20 +1,32 @@
-import { Link } from "react-router-dom"
-
-import {data} from "../../../data/recipes";
+import { Link } from 'react-router-dom';
+import { data } from '../../../data/recipes';
+import './RecipeList.css';
+import { FaStar } from 'react-icons/fa';
 
 const RecipeList = () => {
-  return (
-    <section className="container">
-    {data.map((recipe) => (
-      <div key={recipe.id}>
-        <img src={recipe.image} alt={recipe.name} />
-        <h3>{recipe.name}</h3>
-        <p>{recipe.rating}</p>
-        <Link to={`/recipe/${recipe.id}`}>View</Link>
-      </div>
-    ))}
-  </section>
-  )
-}
+    return (
+        <section className="section">
+            {data.map((recipe) => (
+                <div key={recipe.id} className="container">
+                    <figure className="figure">
+                        <img src={recipe.image} alt={recipe.name} className="image" />
+                    </figure>
+                    <div className="cardContent">
+                        <h3 className="name">{recipe.name}</h3>
+                        <div className="btn-ctn">
+                            <Link to={`/recipe/${recipe.id}`} className="btn">
+                                Get Recipe
+                            </Link>
+                        </div>
+                        <div className="rating">
+                            <FaStar color="#D4AD2E" /> &nbsp;
+                            {recipe.rating}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </section>
+    );
+};
 
-export default RecipeList
+export default RecipeList;
