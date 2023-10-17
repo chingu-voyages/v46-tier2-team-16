b812c6fc2563ad8e3919500d1904de761079caf6
Nisha <nishavijai@gmail.com>
2023-10-17 10:41:20 +0200

    Add redux store setup and files from last week

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,6 @@ const recipesToDisplay = useMemo(() => {
         
       </main>
       <Footer />
-
     </>
   )
 }
--- src/features/recipe/recipeSlice.js
+++ src/features/recipe/recipeSlice.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const TASTY_RAPID_API_URL = import.meta.env.VITE_TASTY_RAPID_API_URL;
 const TASTY_RAPID_API_KEY = import.meta.env.VITE_TASTY_RAPID_API_KEY;
 const TASTY_RAPID_API_HOST = import.meta.env.VITE_TASTY_RAPID_API_HOST;
-
-// import { TASTY_RAPID_API_URL, TASTY_RAPID_API_KEY, TASTY_RAPID_API_HOST } from '../../constants/apiData';
-=======
-import axios from 'axios'
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-
-const TASTY_RAPID_API_URL = import.meta.env.VITE_TASTY_RAPID_API_URL
-const TASTY_RAPID_API_KEY = import.meta.env.VITE_TASTY_RAPID_API_KEY
-const TASTY_RAPID_API_HOST = import.meta.env.VITE_TASTY_RAPID_API_HOST
->>>>>>> faf6a87 (Add env)
 
 const initialState = {
     loading: false,
@@ -39,7 +28,6 @@ const options = {
 export const fetchRecipes = createAsyncThunk('recipe/fetchRecipes', () => {
     return axios.request(options).then((response) => response.data);
 });
-
 
 const recipeSlice = createSlice({
     name: 'recipe',
