924fa25b1c3ce3f141d46616b8e4bba78ea710e9
Celine <celine.lancelle@gmail.com>
2023-10-19 11:12:49 +0200

    chore: link store + search tool

--- src/features/recipe/Recipe.jsx
+++ src/features/recipe/Recipe.jsx
@@ -4,31 +4,30 @@ import { fetchRecipes } from './recipeSlice'
 
 import styles from './Recipe.module.css'
 
-const Recipe = () => {
-    const recipe = useSelector(state => state.recipe)
-    const recipeResults = recipe.recipes.results
-    console.log("Recipe results: ", recipeResults)
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(fetchRecipes())
-    }, [])
-
+// const Recipe = ({recipe, recipesToDisplay}) => {
+    const Recipe = (props) => {
+        const { recipe, recipesToDisplay } = props
+        console.log('recipesToDisplay', recipesToDisplay)
     return (
         <section className={styles.container}>
             <h2 className={styles.text}>List of Recipes</h2>
+
             {recipe.loading && <p className={styles.text}>Loading...</p>}
-            {!recipe.loading && recipe.error ? <p className={styles.text}>Error: {recipe.error}</p> : null}
-            {!recipe.loading && recipeResults ? (
-                <ul>
-                    {recipeResults.map(recipe => (
-                        <li key={recipe.id} className={styles.text}>{recipe.name}</li>
-                    ))}
+            {!recipe.loading && recipe.error ? (
+                <p className={styles.text}>Error: {recipe.error}</p>
+                ) : null}
+            {!recipe.loading && recipesToDisplay ? (
+                 <ul>
+                    {recipesToDisplay.map((recipe) => (
+                        <li key={recipe.id} className={styles.text}>
+                            {recipe.name}
+                        </li>
+                        ))          
+                    }
                 </ul>
-            ) : null}
+                ) : <p>No recipe with this name</p>}
         </section>
     )
 }
 
-export default Recipe
+export default Recipe;
