acfd67d4c264a9b65bc897fd11492d2dde78010c
Mladen Krstin <mladen.krstin@gmail.com>
2023-11-01 17:23:42 +0100

    styled nutrition table

--- src/components/recipeItem/RecipeItem.jsx
+++ src/components/recipeItem/RecipeItem.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './RecipeItem.module.css';
-import { BiSolidLike, BiSolidDislike } from 'react-icons/bi';
+import { BiSolidLike, BiSolidDislike, BiBowlHot } from 'react-icons/bi';
 
-const RecipeItem = ({selectedRecipe}) => {
-    console.log('selectedRecipe', selectedRecipe)
+const RecipeItem = ({ selectedRecipe }) => {
+    console.log('selectedRecipe', selectedRecipe);
 
-    const sections  = selectedRecipe.sections[0].components
+    const sections = selectedRecipe.sections[0].components;
 
     return !!selectedRecipe ? (
         <section className={styles.containerDetails}>
@@ -15,7 +15,9 @@ const RecipeItem = ({selectedRecipe}) => {
                     <div className={styles.detailsCtn}>
                         <div>
                             <h3 className={styles.itemName}>{selectedRecipe.name}</h3>
-                            <h4 className={styles.itemCategory}>Category: {selectedRecipe.total_time_tier?.display_tier}</h4>
+                            <h4 className={styles.itemCategory}>
+                                Category: <span>{selectedRecipe.total_time_tier?.display_tier}</span>
+                            </h4>
                             <p className={styles.itemPrep}>
                                 Preparation Time: <span>{selectedRecipe.prep_time_minutes} minutes</span>
                             </p>
@@ -23,7 +25,10 @@ const RecipeItem = ({selectedRecipe}) => {
                                 Cooking Time: <span>{selectedRecipe.cook_time_minutes} minutes</span>
                             </p>
                             <p className={styles.itemServ}>
-                                Servings: <span>{selectedRecipe.num_servings}</span>
+                                Servings: &nbsp;
+                                <span>
+                                    {selectedRecipe.num_servings} <BiBowlHot />
+                                </span>
                             </p>
                             <p className={styles.itemLike}>
                                 <BiSolidLike color="green" /> <span>{selectedRecipe.user_ratings.count_positive}</span>
@@ -37,8 +42,8 @@ const RecipeItem = ({selectedRecipe}) => {
                                 Ingredients:
                                 <span>
                                     {sections.map((item) => (
-                                    <li key={item.id}>{item.raw_text}</li>))
-                                    }
+                                        <li key={item.id}>{item.raw_text}</li>
+                                    ))}
                                 </span>
                             </p>
                         </div>
@@ -55,28 +60,44 @@ const RecipeItem = ({selectedRecipe}) => {
             </div>
             <div className={styles.facts}>
                 <video controls width="450" className={styles.video}>
-                    <source src={selectedRecipe.video_url} type="application/x-mpegURL" />
+                    <source src={selectedRecipe.original_video_url} type="application/x-mpegURL" />
                 </video>
                 <div className={styles.nutritionContainer}>
-                    <h3 className={styles.nutritionH3}>Nutrition Facts:</h3>
-                    <p className={styles.nutrition}>
-                        Calories: <span>{selectedRecipe.nutrition.calories}</span>
-                    </p>
-                    <p className={styles.nutrition}>
-                        Carbohydrates: <span>{selectedRecipe.nutrition.carbohydrates}</span>
-                    </p>
-                    <p className={styles.nutrition}>
-                        Fat: <span>{selectedRecipe.nutrition.fat}</span>
-                    </p>
-                    <p className={styles.nutrition}>
-                        Fiber: <span>{selectedRecipe.nutrition.fiber}</span>
-                    </p>
-                    <p className={styles.nutrition}>
-                        Protein: <span>{selectedRecipe.nutrition.protein}</span>
-                    </p>
-                    <p className={styles.nutrition}>
-                        Sugar: <span>{selectedRecipe.nutrition.sugar}</span>
-                    </p>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr>
+                                <th className={styles.tableLeft}>Nutritions</th>
+                                <th className={styles.tableRight}>Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className={styles.tableLeft}>Calories:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.calories}kCal</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.tableLeft}>Carbohydrates:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.carbohydrates} g</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.tableLeft}>Fat:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.fat} g</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.tableLeft}>Fiber:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.fiber} g</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.tableLeft}>Protein:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.protein} g</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.tableLeft}>Sugar:</td>
+                                <td className={styles.tableRight}>{selectedRecipe.nutrition.sugar} g</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p className={styles.tableNote}>*per 100g of serving</p>
                 </div>
             </div>
 
@@ -84,6 +105,9 @@ const RecipeItem = ({selectedRecipe}) => {
                 Go Back
             </Link>
         </section>
-) : <></>}
+    ) : (
+        <></>
+    );
+};
 
 export default RecipeItem;
