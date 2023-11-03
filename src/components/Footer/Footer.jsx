79da65275d6a7a648db9dbc71089f5e49fdad061
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-03 14:04:04 +0200

    implement dark mode on the whole app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,57 +1,60 @@
 import styles from './Footer.module.css';
 import { AppLogoComponent } from '../AppLogoComponent/index';
 import { AnchorLink } from '../UI/AnchorLink';
+import { useGlobalContext } from '../../contexts/DarkModeContext';
 
 const Footer = () => {
+    const { isDarkTheme } = useGlobalContext();
+
     return (
-        <footer>
+        <footer className={`${styles.footer} ${isDarkTheme ? styles['dark-footer'] : ''}`}>
             <section className={styles['logo-section']}>
                 <AppLogoComponent />
             </section>
 
-            <section className={styles['links-section']}>
-                <section className={styles['links-first-section']}>
-                    <AnchorLink text="Back to the top" />
-                    <AnchorLink text="My colllection" />
-                    <AnchorLink text="Accessibility" />
-                    <AnchorLink text="Sitemap" />
+            <section className={`${styles['links-section']} ${isDarkTheme ? styles['dark-links-section'] : ''}`}>
+            <section className={styles['links-first-section']}>
+                <AnchorLink text="Back to the top" className={isDarkTheme ? styles['dark-link'] : ''} />
+                <AnchorLink text="My collection" className={isDarkTheme ? styles['dark-link'] : ''} />
+                <AnchorLink text="Accessibility" className={isDarkTheme ? styles['dark-link'] : ''} />
+                <AnchorLink text="Sitemap" className={isDarkTheme ? styles['dark-link'] : ''} />
+            </section>
+            <section className={styles['horizontal-divider-section']}>
+                <hr className={styles['horizontal-divider']}></hr>
+            </section>
+            <section className={styles['links-second-section']}>
+                <section className={styles['links-second-section--first-list']}>
+                    <AnchorLink text="Fish" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Beef" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Chicken" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Meat" className={isDarkTheme ? styles['dark-link'] : ''} />
                 </section>
-                <section className={styles['horizontal-divider-section']}>
-                    <hr className={styles['horizontal-divider']}></hr>
+                <section className={styles['vertical-divider__first--show']}>
+                    <hr className={styles['vertical-divider']}></hr>
                 </section>
-                <section className={styles['links-second-section']}>
-                    <section className={styles['links-second-section--first-list']}>
-                        <AnchorLink text="Fish" />
-                        <AnchorLink text="Beef" />
-                        <AnchorLink text="Chicken" />
-                        <AnchorLink text="Meat" />
-                    </section>
-                    <section className={styles['vertical-divider__first--show']}>
-                        <hr className={styles['vertical-divider']}></hr>
-                    </section>
-                    <section className={styles['links-second-section--second-list']}>
-                        <AnchorLink text="Sides" />
-                        <AnchorLink text="Vegetarian" />
-                        <AnchorLink text="Vegan" />
-                        <AnchorLink text="Deserts" />
-                    </section>
-                    <section className={styles['vertical-divider__second--hide']}>
-                        <hr className={styles['vertical-divider__second']}></hr>
-                    </section>
-                    <section className={styles['links-second-section--third-list']}>
-                        <AnchorLink text="Sides" />
-                        <AnchorLink text="Vegetarian" />
-                        <AnchorLink text="Vegan" />
-                        <AnchorLink text="Deserts" />
-                    </section>
+                <section className={styles['links-second-section--second-list']}>
+                    <AnchorLink text="Sides" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Vegetarian" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Vegan" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Desserts" className={isDarkTheme ? styles['dark-link'] : ''} />
+                </section>
+                <section className={styles['vertical-divider__second--hide']}>
+                    <hr className={styles['vertical-divider__second']}></hr>
+                </section>
+                <section className={styles['links-second-section--third-list']}>
+                    <AnchorLink text="Sides" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Vegetarian" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Vegan" className={isDarkTheme ? styles['dark-link'] : ''} />
+                    <AnchorLink text="Desserts" className={isDarkTheme ? styles['dark-link'] : ''} />
                 </section>
             </section>
+        </section>
 
             <section className={styles['copyright-section']}>
-                <p className={styles.text}>
+                <p className={isDarkTheme ? styles['dark-link'] : ''}>
                     Developed by{' '}
                     <a
-                        className={styles.textA}
+                    className={isDarkTheme ? styles['dark-link'] : ''}
                         target="_blank"
                         rel="noreferrer"
                         href="https://github.com/chingu-voyages/v46-tier2-team-16/tree/main"
