f6532e8e24a28c1851ad7692fc2d34a48a718191
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-05 19:48:56 +0200

    fix dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,18 @@ import store from './app/store'
 import App from './App.jsx'
 import './index.css'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
-import { recipeSlice } from './features/recipe/recipeSlice'
+import { AppProvider } from './contexts/DarkModeContext.jsx'
+// import { recipeSlice } from './features/recipe/recipeSlice'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AppProvider>
+      <ApiProvider>
     <Provider store={store}>
-      <App dark-theme='dark' />
+      <App />
     </Provider>
+    </ApiProvider>
+    </AppProvider>
   </React.StrictMode>
 )
+
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -5,18 +5,14 @@ import styles from './NotFound.module.css';
 import { useGlobalContext } from '../../contexts/DarkModeContext';
 
 const NotFound = () => {
-    // const { isDarkTheme } = useGlobalContext();
+    const { isDarkTheme } = useGlobalContext();
     return (
         <section className={styles.section}>
             <AppLogoComponent className={styles.logo} />
-            <p>This is not the page you are looking for!</p>
-            <Link to="/" >
-                Go back Home
-            </Link>
-            {/* <p className={`${styles.content} ${isDarkTheme ? styles['dark-content'] : ''}`}>This is not the page you are looking for!</p>
+            <p className={`${styles.content} ${isDarkTheme ? styles['dark-content'] : ''}`}>This is not the page you are looking for!</p>
             <Link to="/" className={`${styles.button} ${isDarkTheme ? styles['dark-button'] : ''}`}>
                 Go back Home
-            </Link> */}
+            </Link> 
         </section>
     );
 };
