27cf6d9b5ebb4d266699529b632f8c19dcb11685
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-06 15:12:53 +0200

    hotfix: import NotFound.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { HomeLayout } from './layout';
 import { NotFound } from './pages/NotFound';
 import { RecipeDetails } from './pages/recipeDetails';
-import {Home} from "./pages/Home"
+import { Home } from "./pages/Home"
 
 const router = createBrowserRouter([
     {
