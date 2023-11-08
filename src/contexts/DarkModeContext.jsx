514b655f86487feb19cbd9b685b9e24f22678922
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-08 20:28:34 +0200

    fix dark mode for 404 page

--- src/contexts/DarkModeContext.jsx
+++ src/contexts/DarkModeContext.jsx
@@ -2,18 +2,10 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
-const getInitialDarkMode = () => {
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme:dark)"
-  ).matches;
-  const storedDarkMode = localStorage.getItem("darkTheme") === "true";
-
-  return storedDarkMode || prefersDarkMode;
-};
 
 // eslint-disable-next-line react/prop-types
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [isDarkTheme, setIsDarkTheme] = useState();
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
