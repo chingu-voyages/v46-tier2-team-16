91aa67275b847a2809252a91ca76d89e9bce05de
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-08 22:53:30 +0200

    fixing bug by changing page design

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,18 +1,16 @@
 import { Link } from 'react-router-dom';
 
-import { AppLogoComponent } from '../../components/AppLogoComponent';
 import styles from './NotFound.module.css';
-import { useGlobalContext } from '../../contexts/DarkModeContext';
 
 const NotFound = () => {
-    const { isDarkTheme } = useGlobalContext();
     return (
         <section className={styles.section}>
-            <AppLogoComponent className={styles.logo} />
-            <p className={`${styles.content} ${isDarkTheme ? styles['dark-content'] : ''}`}>This is not the page you are looking for!</p>
-            <Link to="/" className={`${styles.button} ${isDarkTheme ? styles['dark-button'] : ''}`}>
+            <div className={styles.container}>
+            <p className={styles.content} >This is not the page you are looking for!</p>
+            <Link to="/" className={styles.button}>
                 Go back Home
             </Link> 
+            </div>
         </section>
     );
 };
