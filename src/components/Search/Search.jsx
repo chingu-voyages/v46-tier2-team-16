9c7df4524e058e10538bc1f3cfbcd0625fbe07a7
Celine <celine.lancelle@gmail.com>
2023-11-08 19:18:33 +0100

    bugfix: fix search bar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react';
 import styles from './Search.module.css';
-import Reset from "../../assets/images/iconsReset.png";
+import Reset from '../../assets/images/iconsReset.png';
 import { useGlobalContext } from '../../contexts/DarkModeContext';
 
 const Search = (props) => {
-    const { handleSearch, handleQuickCooking } = props;
+    const { handleSearch } = props;
     const [newSearch, setNewSearch] = useState('');
 
     const { isDarkTheme } = useGlobalContext();
 
-    const handleInput = (e) => {
+    const handleClick = (e) => {
         e.preventDefault();
-
-        const input = e.target.value;
-        setNewSearch(input);
-        handleSearch(input);
+        handleSearch(newSearch);
     };
 
-    const handleClick = () => {
+    const handleKeyUp = (e) => {
+        if (e.key === 'Enter') handleSearch(newSearch);
+    };
+
+    const handleChange = (e) => {
+        setNewSearch(e.target.value);
+    };
+
+    const handleClickReset = () => {
         setNewSearch('');
+        handleSearch('');
     };
 
     return (
         <div className={styles.container}>
-            <h2 className={`${styles.text} ${isDarkTheme ? styles['dark-text'] : ''}`}>
-                Recipes
-            </h2>
+            <h2 className={`${styles.text} ${isDarkTheme ? styles['dark-text'] : ''}`}>Recipes</h2>
             <span className={styles.search}>
-                <input id="input" type="text" placeholder="Discover recipes" value={newSearch} onChange={(e) => handleInput(e)} />
-                <button onClick={handleClick}>
+                <label htmlFor="search">Search</label>
+                <input
+                    id="input"
+                    name="search"
+                    type="text"
+                    placeholder="e.g. avocado, tomatoes"
+                    value={newSearch}
+                    onChange={handleChange}
+                    onKeyUp={handleKeyUp}
+                />
+                <button onClick={handleClick}>Search</button>
+                <button onClick={handleClickReset}>
                     <img className={styles.img} src={Reset} alt="reset search bar" />
                 </button>
             </span>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,49 +2,80 @@ import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Search } from '../../components/Search';
 import { Recipe } from '../../components/Recipe';
+import { Loader } from '../../components/Loader';
 import { fetchRecipes } from '../../features/slice/recipeSlice';
 
 const Home = () => {
+    const dispatch = useDispatch();
+
     const recipe = useSelector((state) => state.recipe);
     const recipeResults = recipe.recipes.results;
 
-    const dispatch = useDispatch();
+    const [newSearch, setNewSearch] = useState('');
+    // will need to map through each recipe and then map through  components  and compare the search with ingredient.name
+    // we should not search for exact term but part of the term
+    const [filteredByIngredient, setFilteredByIngredient] = useState([]);
 
     useEffect(() => {
         dispatch(fetchRecipes());
     }, [dispatch]);
 
-    const [filteredByName, setFilteredByName] = useState([]);
-    const [newSearch, setNewSearch] = useState('');
-
     const handleSearch = (newSearch) => {
         setNewSearch(newSearch);
-        const filteredByName = recipeResults.filter((recipe) => recipe.name.toLowerCase().includes(newSearch.toLowerCase()));
-        setFilteredByName(filteredByName);
+        searchByIngredient(newSearch);
     };
 
-    const [filteredByCookingTime, setFilteredByCookingTime] = useState([]);
+    const searchByIngredient = (newSearch) => {
+        if (newSearch === '') {
+            setFilteredByIngredient([]);
+        } else {
+            // split search in array
+            const searchIngredients = newSearch.split(/[\s,; ]+/);
 
-    const handleQuickCooking = () => {
-        const filteredByCookingTime = recipeResults.filter((recipe) => recipe.total_time_tier.tier === 'under_15_minutes');
-        setFilteredByCookingTime(filteredByCookingTime);
+            const filteredRecipes = recipeResults.filter((recipe) => {
+                // extract all ingredient names from current recipe
+                const recipeIngredients = recipe.sections[0].components.map((component) => component.ingredient.name);
+
+                // true if ALL search ingredients are found in recipe
+                return searchIngredients.every((searchIngredient) => {
+                    // for each search ingredient, true if at least ONE ingredient is found
+                    return recipeIngredients.some((recipeIngredient) => {
+                        return recipeIngredient.toLowerCase().includes(searchIngredient.toLowerCase());
+                    });
+                });
+            });
+
+            setFilteredByIngredient(filteredRecipes);
+        }
     };
 
     const recipesToDisplay = useMemo(() => {
-        if (newSearch === '' && filteredByName.length === 0 && filteredByCookingTime.length === 0) {
-            return recipeResults;
-        } else if (filteredByName.length !== 0) {
-            return filteredByName;
-        } else if (newSearch !== '' && filteredByName.length === 0) {
+        if (newSearch === '' && filteredByIngredient.length === 0) {
+            return recipeResults || [];
+        } else if (newSearch !== '' && filteredByIngredient.length !== 0) {
+            return filteredByIngredient;
+        } else if (newSearch !== '' && filteredByIngredient.length === 0) {
             return [];
-        } else if (filteredByCookingTime.length !== 0) {
-            return filteredByCookingTime;
         }
-    }, [newSearch, recipeResults, filteredByName, filteredByCookingTime]);
+    }, [recipeResults, newSearch, filteredByIngredient]);
+
+    // const accessIngredients = selectedRecipe.sections[0].components
+    // console.log('accessIngredients', accessIngredients)
+
+    // const recipesToDisplay = useMemo(() => {
+    //     if (newSearch === '' && filteredByName.length === 0) {
+    //         return recipeResults;
+    //     } else if (filteredByName.length !== 0) {
+    //         return filteredByName;
+    //     } else if (newSearch !== '' && filteredByName.length === 0) {
+    //         return [];
+    //     }
+    // }, [newSearch, recipeResults, filteredByName]);
 
     return (
         <>
-            <Search handleSearch={handleSearch} handleQuickCooking={handleQuickCooking} />
+            <Search handleSearch={handleSearch} />
+            {recipe.loading && <Loader />}
             {recipeResults ? <Recipe recipe={recipeResults} recipesToDisplay={recipesToDisplay} /> : <></>}
         </>
     );
