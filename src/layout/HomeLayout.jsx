ce2ec233e05d41a74eda8634b6153bc0512381aa
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-11-10 12:07:32 +0200

    hotfix: fix bg color on light theme

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,21 +1,21 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
-import styles from './HomeLayout.module.css';
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import styles from "./HomeLayout.module.css";
 import { useGlobalContext } from '../contexts/DarkModeContext';
 
 const HomeLayout = () => {
-    const { isDarkTheme } = useGlobalContext();
+  const { isDarkTheme } = useGlobalContext();
 
-    return (
-        <>
-            <ScrollRestoration />
-            <Header />
-            <main className={`${styles.main} ${isDarkTheme ? styles['dark-theme'] : ''}`}>
-                <Outlet className={styles.outlet} />
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className={`${styles.main} ${isDarkTheme ? styles['dark-theme'] : styles['light-theme']}`}>
+        <Outlet className={styles.outlet} />
+      </main>
+      <Footer />
+    </>
+  );
 };
+
 export default HomeLayout;
